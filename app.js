add06b6b69bd584d241d9c395df7ed4f4bede2a5
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-06-19 21:07:28 +0200

    hello, witam, natalia

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+// ...var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var witamRouter = require('./routes/witam');
+var nataliaRouter = require('./routes/natalia');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/hello/witam', witamRouter);
+app.use('/hello/natalia', nataliaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
