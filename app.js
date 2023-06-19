0d0d0a76365da63a4167988196d147d9c96b9fa1
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-06-19 22:55:38 +0200

    the absolute path instead of a relative one

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// ...var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const path = require('path');
+
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var express = require('express');
+//var path = require('path');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +13,6 @@ var witamRouter = require('./routes/witam');
 var nataliaRouter = require('./routes/natalia');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
