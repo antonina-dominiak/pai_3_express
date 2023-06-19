1b13180456b1e8f48cb61a8f316fffd2671bb705
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-06-19 21:18:39 +0200

    works for any name now

--- routes/hello.js
+++ routes/hello.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    var name = req.query.name || 'Guest';
-    res.send('witaj michael!');
+    var name = req.query.name || 'Name';
+    res.send('witaj ' + name + '!');
  });
 
  module.exports = router;
